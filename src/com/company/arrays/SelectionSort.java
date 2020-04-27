e02f33228c2192a3eb2a301a573cd6772683846f
sergey <s.kholstinin@yandex.ru>
2020-04-27 16:52:21 +0300

    Initial commit

---
+++ src/com/company/arrays/SelectionSort.java
@@ -0,0 +1,30 @@
+package com.company.arrays;
+
+import static com.company.arrays.BubbleSort.swap;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] intArray = new int[7];
+        intArray[0] = 20;
+        intArray[1] = 35;
+        intArray[2] = -15;
+        intArray[3] = 7;
+        intArray[4] = 55;
+        intArray[5] = 1;
+        intArray[6] = -22;
+
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            int largest = 0;
+            for (int i = 1; i <= lastUnsortedIndex; i++) {
+                if (intArray[i] > intArray[largest]) {
+                    largest = i;
+                }
+            }
+            swap(intArray, largest, lastUnsortedIndex);
+        }
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+}
